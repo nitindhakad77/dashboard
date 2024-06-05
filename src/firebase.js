68132c63b743b36fe3e52a3dd23af1b7a7d9e492
nitindhakad77 <95351272+nitindhakad77@users.noreply.github.com>
2024-06-05 12:51:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import {getFirestore} from "firebase/firestore";
 // console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBJ4q0lSnMbzIkIdmkrBlFKVAg36rWZ-C8",
+  authDomain: "moviex-react-5cae6.firebaseapp.com",
+  projectId: "moviex-react-5cae6",
+  storageBucket: "moviex-react-5cae6.appspot.com",
+  messagingSenderId: "534458219869",
+  appId: "1:534458219869:web:8f66269e54bf7fa8487cb4",
+  measurementId: "G-6G4E0Q5VSQ"
 };
 
 const app = initializeApp(firebaseConfig);
